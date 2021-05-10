4d6aede04b36d45465444517305b22b463a1d187
Jeremy <jeremy.markle@gmail.com>
2021-05-10 06:19:36 -0400

    change to broadcast

--- root/app/main.py
+++ root/app/main.py
@@ -42,7 +42,7 @@ def publish():
     """Posts a message to the pubsub topic"""
     if request.method == "POST":
         data = request.json
-    socketio.emit('message', {'data': data}, namespace=f"/{TOPIC}")
+    socketio.emit('message', {'data': data}, broadcast=True)
     return Response(status=204)
 
 @socketio.on('connect')
